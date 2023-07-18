b8b6f9b776f591ff778eed406b2b14a8845cc1e6
Jesper Martinsson <jesper.martinsson92@gmail.com>
2023-07-18 17:11:20 +0200

    Started mobile menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import { NavLink } from "./NavLink"
+import { GiHamburgerMenu } from "react-icons/gi";
 
 
 const links = [
@@ -12,11 +13,15 @@ const links = [
 export const Navbar = () => {
 
     return (
-        <nav className="w-full justify-center gap-24 self-center h-full roboto hidden md:flex">
-            {links.map((link:any, index:number) => {
+        <><nav className="w-full justify-center gap-24 self-center h-full roboto hidden md:flex">
+            {links.map((link: any, index: number) => {
                 return (<NavLink key={index} route={link.route} label={link.label} />)
             })}
         </nav>
+        <div className="px-2 flex md:hidden justify-between">
+            <GiHamburgerMenu className="w-8 h-8" />
+            <p className="text-2xl font-bold roboto px-5">martinsson.io</p>
+            </div></>
     )
 
 }
