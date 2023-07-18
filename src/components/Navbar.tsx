eefc7cbf3845293755fd3cdb9c7e3f5fa73f4ded
Jesper Martinsson <jesper.martinsson92@gmail.com>
2023-07-18 22:45:24 +0200

    Fixed mobile navbar UI

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { NavLink } from "./NavLink"
 import { GiHamburgerMenu } from "react-icons/gi";
 import { GrClose } from "react-icons/gr";
+import { IoMdClose } from "react-icons/io";
 
 
 const links = [
@@ -28,12 +29,12 @@ export const Navbar = () => {
                 return (<NavLink key={index} route={link.route} label={link.label} />)
             })}
         </nav>
-        <div className="px-2 flex md:hidden justify-between">
-            {!showMenu ? <GiHamburgerMenu onClick={toggleMenu} className="w-8 h-8 cursor-pointer hover:text-indigo-500" /> : <GrClose onClick={toggleMenu} className="w-8 h-8 cursor-pointer hover:text-indigo-500" />}
+        <div className="px-2 flex md:hidden justify-between self-center">
+            {!showMenu ? <GiHamburgerMenu onClick={toggleMenu} className="w-8 h-8 cursor-pointer hover:text-indigo-500 self-center" /> : <IoMdClose onClick={toggleMenu} className="w-8 h-8 cursor-pointer hover:text-indigo-500 my-auto" />}
             <p className="text-2xl font-bold roboto px-5">martinsson.io</p>
             </div>
             
-            {showMenu && <div className="absolute flex md:hidden z-20 bg-white p-3 w-full">
+            {showMenu && <div className="absolute mt-5 flex md:hidden z-20 bg-white p-3 w-full dark:bg-black border-t-2 border-indigo-600">
 
 <div className="flex flex-col justify-start gap-1">
 {links.map((link: any, index: number) => {
