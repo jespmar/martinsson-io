234f6a982346ae1d21e36c2109f662e3cd03709c
Jesper Martinsson <jesper.martinsson92@gmail.com>
2023-07-24 23:51:09 +0200

    New sync script

--- sync_post_images.sh
+++ sync_post_images.sh
@@ -1,1 +1,5 @@
-rsync -avh --include='*/' --include='*.jpg' --include='*.png' --include='*.gif' --include='*.jpeg' --include='*.svg' --exclude='*' _posts/ public/assets/blog
+#rsync -avh --include='*/' --include='*.jpg' --include='*.png' --include='*.gif' --include='*.jpeg' --include='*.svg' --exclude='*' _posts/ public/assets/blog
+
+cd _posts
+find . -name '*.jpg' | cpio -pdm  ../public/assets/blog
+cd ../
