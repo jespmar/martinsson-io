402e35b58970fd28341927fbc0e932157ea5ddaf
Jesper Martinsson <jesper.martinsson92@gmail.com>
2023-07-26 18:46:09 +0200

    Removed image URL text

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -14,7 +14,6 @@ export default async function About() {
         <Layout>
 <div className="text-center text-5xl roboto font-bold underline my-10 dark:text-white">{page.title}</div>
 <Image className='w-full' width={1000} height={1000} src={`${page.coverImage}`} alt="cover image" />
-<p className='text-xs text-gray-500 mb-3 mt-2'>{`${page.coverImage}`}</p>
 
 {/* <BlogPost elements={elements} /> */}
 <div
